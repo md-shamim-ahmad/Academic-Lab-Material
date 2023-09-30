6048717c15c7d174fa76e66d4cb978bc0408cf4d
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-09-30 17:08:50 +0600

    Update 2. Shortest Job First CPU Scheduling Algorithm.cpp

--- Operating System/CPU Scheduling Algorithm/2. Shortest Job First CPU Scheduling Algorithm.cpp
+++ Operating System/CPU Scheduling Algorithm/2. Shortest Job First CPU Scheduling Algorithm.cpp
@@ -1,53 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct processor {
+
+struct items{
     string name;
-    int AT; // Arrtival Time
-    int BT; // Brust Time
-    int CT; // Completion Time
-    int TAT; // Turn Around Time
-    int WT; // Waiting Time
+    int index, at, bt, ct, tat, wt;
+    bool complete = false;
 };
-void calculate_CompletionTime(vector<processor> &processorList) {
-    processorList[0].CT = processorList[0].AT + processorList[0].BT;
-    for (int i = 1; i < processorList.size(); i++)
-        processorList[i].CT = (processorList[i].AT <= processorList[i - 1].CT ? processorList[i].BT +
-                processorList[i - 1].CT : processorList[i].BT + processorList[i].AT);
+void CompletionTime(vector<items> &process) {
+    process[0].ct = process[0].at + process[0].bt;
+    process[0].complete = true;
+    for (int i = 1; i < process.size(); i++) {
+        vector<items> temp;
+        int ct = process[i - 1].ct;
+        for (int j = 0; j < process.size(); j++) {
+            if (!process[j].complete && process[j].at <= ct) {
+                temp.push_back(process[j]);
+            }
+        }
+        sort(temp.begin(), temp.end(), [](items p1, items p2){
+            if (p1.bt == p2.bt)
+                return p1.at < p2.at;
+            return p1.bt < p2.bt;
+        });
+        for(int j = 0, k = i; j < temp.size(); j++) {
+            process[k++] = temp[j];
+        }
+        process[i].complete = true;
+        process[i].ct = (process[i].at <= process[i - 1].ct ?
+                         process[i - 1].ct + process[i].bt : process[i].bt + process[i].at);
+    }
 }
-void calculate_TurnAroundTime(vector<processor> &processorList) {
-    for (auto &processor: processorList)
-        processor.TAT = processor.CT - processor.AT;
+void TurnAroundTime(vector<items> &process) {
+    for (int i = 0; i < process.size(); i++) {
+        process[i].tat = process[i].ct - process[i].at;
+    }
 }
-void calculate_WaitingTime(vector<processor> &processorList) {
-    for (auto &processor: processorList)
-        processor.WT = processor.TAT - processor.BT;
+void WaitingTime(vector<items> &process) {
+    for (int i = 0; i < process.size(); i++) {
+        process[i].wt = process[i].tat - process[i].bt;
+    }
 }
-void SJF_Algorithm(vector<processor> &processorList) {
-    sort(processorList.begin(), processorList.end(), [](processor &a, processor &b) {
-        return a.BT < b.BT;
+void shortestJobFirst(vector<items> &process) {
+    sort(process.begin(), process.end(), [](items a, items b){
+        return a.at < b.at;
+    });
+    CompletionTime(process);
+    TurnAroundTime(process);
+    WaitingTime(process);
+
+    sort(process.begin(), process.end(), [](items a, items b){
+        return a.index < b.index;
     });
 }
+
 int main() {
     int n;
-    cout << "Enter the number of Processor : ";
+    cout << "Enter the numbe of process: ";
     cin >> n;
-    vector<processor> processorList(n);
-    vector<pair<int, int>> pairs;
+    vector<items> process(n);
     for (int i = 0; i < n; i++) {
-        cout << "Enter the Processor Name : ";
-        cin >> processorList[i].name;
-        cout << "Enter the Processor Arival Time : ";
-        cin >> processorList[i].AT;
-        cout << "Enter the Processor Brust Time : ";
-        cin >> processorList[i].BT;
+        process[i].index = i;
+        cout << "Enter process name, arrival time and brust time: ";
+        cin >> process[i].name >> process[i].at >> process[i].bt;
     }
-    SJF_Algorithm(processorList);
-    calculate_CompletionTime(processorList);
-    calculate_TurnAroundTime(processorList);
-    calculate_WaitingTime(processorList);
-    for (auto &processor: processorList) {
-        cout << processor.name << " " << processor.AT << " " << processor.BT << " "
-             << processor.CT << " " << processor.TAT << " " << processor.WT << '\n';
+    shortestJobFirst(process);
+
+    cout << "name\t at\t bt\t ct\t tat\t wt\n";
+    for (auto it : process) {
+        cout << it.name << '\t' << it.at << '\t' << it.bt << '\t' <<
+             it.ct << '\t' << it.tat << '\t' << it.wt << '\n';
     }
-    return 0;
 }
