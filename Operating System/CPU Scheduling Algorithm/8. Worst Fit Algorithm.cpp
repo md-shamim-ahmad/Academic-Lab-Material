b0cf1b57782eb87f86c1c3edebd76c33eb4f4755
Md Shamim Ahmad <56692841+md-shamim-ahmad@users.noreply.github.com>
2023-10-01 22:49:59 +0600

    Update 8. Worst Fit Algorithm.cpp

--- Operating System/CPU Scheduling Algorithm/8. Worst Fit Algorithm.cpp
+++ Operating System/CPU Scheduling Algorithm/8. Worst Fit Algorithm.cpp
@@ -1,54 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Partition {
-    int id;
-    int size;
-    bool allocated;
+struct items{
+    int id, size;
+    string block_num = "N/A";
 };
 
-void worstFit(vector<Partition>& partitions, int processSize) {
-    int worstFitIdx = -1;
-
-    for (int i = 0; i < partitions.size(); ++i) {
-        if (!partitions[i].allocated && partitions[i].size >= processSize) {
-            if (worstFitIdx == -1 || partitions[i].size > partitions[worstFitIdx].size) {
-                worstFitIdx = i;
+void worstFit(vector<items> &process, vector<int> &block) {
+    for (int i = 0; i < process.size(); i++) {
+        int pos = 0, mn = INT_MIN;
+        bool check = false;
+        for (int j = 0; j < block.size(); j++) {
+            if (process[i].size <= block[j]) {
+                if (block[j] - process[i].size > mn) {
+                    mn = block[j] - process[i].size;
+                    pos = j;
+                    check = true;
+                }
             }
         }
-    }
-
-    if (worstFitIdx != -1) {
-        partitions[worstFitIdx].allocated = true;
-        cout << "Process allocated to Partition " << partitions[worstFitIdx].id << endl;
-    } else {
-        cout << "No suitable partition found for the process." << endl;
+        if (check) {
+            process[i].block_num = to_string(pos + 1);
+            block[pos] -= process[i].size;
+        }
     }
 }
 
 int main() {
-    int numPartitions, numProcesses;
-
-    cout << "Enter the number of partitions: ";
-    cin >> numPartitions;
-
-    vector<Partition> partitions(numPartitions);
-
-    for (int i = 0; i < numPartitions; i++) {
-        cout << "Enter the size of Partition " << i + 1 << ": ";
-        cin >> partitions[i].size;
-        partitions[i].id = i + 1;
-        partitions[i].allocated = false;
+    int n;
+    cout << "Enter number of process: ";
+    cin >> n;
+    vector<items> process(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter size of the process: ";
+        cin >> process[i].size;
+        process[i].id = i + 1;
+    }
+    cout << "Enter number of block: ";
+    cin >> n;
+    vector<int> block(n);
+    for (int i = 0; i < n; i++) {
+        cout << "Enter size of the block: ";
+        cin >> block[i];
     }
 
-    cout << "Enter the number of processes: ";
-    cin >> numProcesses;
+    worstFit(process, block);
 
-    for (int i = 0; i < numProcesses; ++i) {
-        int processSize;
-        cout << "Enter the size of Process " << i + 1 << ": ";
-        cin >> processSize;
-        worstFit(partitions, processSize);
+    cout << "Process ID\t Process Size\t Block Number\n";
+    for(int i = 0; i < process.size(); i++) {
+        cout << process[i].id << "\t\t" << process[i].size << "\t\t" << process[i].block_num << '\n';
     }
-    return 0;
 }
